7c74370e98e66568d8c1d9c8d49dfb26508d6e68
henriquericordi <henriquericordi@gmail.com>
2018-04-12 14:31:51 -0300

    Add files via upload
    Initial Version

---
+++ translator/mini_lex.cpp
@@ -0,0 +1,128 @@
+#include "cstdio"
+#include "string"
+#include "iostream"
+#include "cstdlib"
+
+// constantes para types dos tokens
+#define NUM 256
+#define PLUS 257
+#define MINUS 258
+#define ERR 1000
+
+using namespace std;
+
+// estrutura de representação dos Tokens
+struct token
+{
+    int type;
+    double value;
+};
+
+// entrada para o Lex
+string input;
+// posição atual (char) em input
+int pos = 0;
+// char auxiliar para "manter" o último símbolo
+char c = EOF;
+// contagem da linha atual
+int current_line = 0;
+
+// retorno o type do token (recebe um int)
+string token_name(int t)
+{
+    switch (t)
+    {
+    case NUM:
+        return "NUM";
+    case PLUS:
+        return "PLUS";
+    case MINUS:
+        return "MINUS";
+    }
+    return "ERROR";
+}
+
+// faz a leitura de um char em input
+char get_char()
+{
+    if (pos < input.length())
+    {
+        return input[pos++];
+    }
+    else
+    {
+        return EOF;
+    }
+}
+
+// faz a identificação do próximo Token na input e o retorna
+token next_token()
+{
+    token t;
+    char peek;
+    // manter o último símbolo ou requisitar um novo
+    if (c == EOF)
+    {
+        peek = get_char();
+    }
+    else
+    {
+        peek = c;
+        c = EOF;
+    }
+    //ignorar espaços e tabulações
+    do
+    {
+        if (peek == ' ' || peek == '\t')
+            continue;
+        else if (peek == '\n')
+            current_line++;
+        else
+            break;
+    } while (peek = get_char());
+    // identificação de inteiros e reais
+    if (isdigit(peek))
+    {
+        int v = 0;
+        double r = 0.0;
+        do
+        {
+            v = v * 10 + atoi(&peek);
+            peek = get_char();
+        } while (isdigit(peek));
+        // continuação como real
+        if (peek == '.')
+        {
+            peek = get_char();
+            double factor = 1.0;
+            do
+            {
+                //r *= factor;
+                factor /= 10.0;
+                r += atoi(&peek) * factor;
+                peek = get_char();
+            } while (isdigit(peek));
+        }
+        double number = (double)v + r;
+        t.type = NUM;
+        t.value = number;
+        c = peek;
+    }
+    else if (peek == '+')
+    {
+        t.type = PLUS;
+    }
+    else if (peek == '-')
+    {
+        t.type = MINUS;
+    }
+    else if (peek == EOF)
+    {
+        t.type = EOF;
+    }
+    else
+    {
+        t.type = ERR;
+    }
+    return t;
+}
---
+++ translator/simple.cpp
@@ -0,0 +1,68 @@
+#include <cstdio>
+#include <iostream>
+#include <cctype>
+#include <cstdlib>
+#include "mini_lex.cpp"
+
+using namespace std;
+
+void list();
+void term();
+void x();
+void match(int);
+void print(char);
+
+token lookahead;
+
+void print_char(char c){
+    printf("%c ", c);
+}
+
+void print_int(double i){
+    printf("%f ", i);
+}
+
+void list(){
+    term();
+    x();
+}
+
+void x(){
+    if (lookahead.type == PLUS) {
+        match(PLUS); term(); print_char('+'); x();
+    } else if (lookahead.type == MINUS){
+        match(MINUS); term(); print_char('-'); x();
+    } else if (lookahead.type == EOF){
+        printf("\nSuccess.\n");
+    } else {
+        printf("Syntax error on x\n");
+        exit(1);
+    }
+}
+
+void term(){
+    // if (isalpha(lookahead)){ //var
+    //     match(lookahead);
+    // } else
+    if (lookahead.type == NUM) {
+        print_int(lookahead.value);
+        match(NUM);
+    } else {
+        printf("Syntax error on term\n");
+        exit(1);
+    }
+}
+
+void match(int type){
+    if (lookahead.type == type){
+        lookahead = next_token();
+    } else {
+        printf("Match error");
+    }
+}
+
+int main(){
+    input = "12.36-321+111";
+    lookahead = next_token();
+    list();
+}
